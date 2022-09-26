e468ba106a5ad150a1e11da833c911316aef4d4d
pserranouma <114354511+pserranouma@users.noreply.github.com>
2022-09-26 22:32:36 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,25 @@
+import socketserver
+import http.server
+import requests
+import configuration
+
+class server:
+    def __init__(self, config, port = 8081, faasserver = '127.0.0.1', faasport = 8080):
+        server.port = port
+        server.faasserver = faasserver
+        server.faasport = faasport
+        server.config = config
+
+    class handler(http.server.SimpleHTTPRequestHandler):
+        def do_GET(self):
+            fname = server.config.getFunction(self.path[1:])
+            self.send_response(200)
+            self.end_headers()
+            print('Requesting function ' + fname)
+            #r = requests.get("http://" + faasserver + ":" + str(server.faasport) + '/' + fname)
+            #print(r.text)
+
+    def start(self):
+        with socketserver.ThreadingTCPServer(('', server.port), server.handler) as httpd:
+            print("Server started")
+            httpd.serve_forever()
---
+++ serverqos.py
@@ -0,0 +1,21 @@
+import socketserver
+import http.server
+import requests
+import configuration
+
+class serverqos:
+    def __init__(self, config, port = 8082):
+        serverqos.port = port
+        serverqos.config = config
+
+    class handler(http.server.SimpleHTTPRequestHandler):
+        def do_GET(self):
+            request = self.path[1:]
+            self.send_response(200)
+            self.end_headers()
+            print('QoS set to ' + request)
+
+    def start(self):
+        with socketserver.ThreadingTCPServer(('', server.port), server.handler) as httpd:
+            print("QoS server started")
+            httpd.serve_forever()
